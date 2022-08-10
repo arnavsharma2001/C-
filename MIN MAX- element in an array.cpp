53052981230dec8de62ac4c70d08ebd84ffad8cf
arnavsharma2001 <73438357+arnavsharma2001@users.noreply.github.com>
2022-08-10 23:45:21 +0530

    Create MIN MAX- element in an array.cpp

---
+++ MIN MAX- element in an array.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+
+int getMax(int num[], int n){
+    
+    int max = INT_MIN;
+    
+    for(int i=0; i<n;i++){
+    if(num[i]> max) {
+        max =num[i];
+    
+ 
+}
+}
+
+// returning max value
+
+return max;
+}
+
+int getMin(int num[], int n){
+    
+    int min = INT_MAX;
+    
+    for(int i=0; i<n;i++){
+    if(num[i]< min){
+    min =num[i];
+  
+}
+}
+
+// returning min value
+
+return min;
+}
+
+
+int main(){
+    
+    int size;
+    cin>>size;
+    
+    int num[1000];
+    
+    
+    // input array
+    for(int i =0; i<size; i++){
+    
+    cin>> num[i];
+    }
+    
+    cout<<"min is "<<getMin(num,size)<<endl;
+    cout<<"max is "<<getMax(num,size);
+    return 0;
+}
