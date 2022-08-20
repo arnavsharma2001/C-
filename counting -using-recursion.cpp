ff175b17935f5643d2e059bbb29aa3080f836fb2
arnavsharma2001 <73438357+arnavsharma2001@users.noreply.github.com>
2022-08-20 10:57:24 +0530

    Create counting -using-recursion.cpp

---
+++ counting -using-recursion.cpp
@@ -0,0 +1,55 @@
+//my approach of counting using recursion
+
+#include <iostream>
+using namespace std;
+
+int count(int n){
+    if(n==0){
+        return 1;
+        }
+
+        int small_problem= count(n-1);
+        cout<<small_problem<<endl;
+        int big_problem = 1+small_problem;
+        
+        return big_problem;
+}
+
+int main() {
+  
+  int n;  
+ cin>>n;
+ 
+ int ans= count(n-1);
+ cout<<ans<<endl;
+
+    return 0;
+}
+
+
+//correct approach
+//gives reverse output
+#include <iostream>
+using namespace std;
+
+void print(int n){
+    if(n==0){
+        return;
+    }
+    cout<<n<<endl;
+    print(n-1);
+    
+}
+
+
+int main() {
+  
+  int n;  
+ cin>>n;
+ 
+
+ cout<<endl;
+ print(n);
+
+    return 0;
+}
