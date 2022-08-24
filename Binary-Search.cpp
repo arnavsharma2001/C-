40701fbd70307a5633ae30e0a2a01153774e0e5f
arnavsharma2001 <73438357+arnavsharma2001@users.noreply.github.com>
2022-08-24 16:15:11 +0530

    Create Binary-Search.cpp

---
+++ Binary-Search.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int BinarySearch(int arr[] , int size, int key){
+    
+    int start =0;
+    int end = size-1;
+    int mid = (start+end)/2;
+    
+    
+    while(start<=end){
+        
+        if(arr[mid]==key){
+            return mid;
+        }
+        
+        //shifting to right part
+        if(arr[mid]<key){
+            start = mid+1;
+        }
+        
+        
+        //shifting to left part
+         else{
+           end = mid-1;
+        }
+        
+        
+        mid = (start+end)/2;
+    }
+    
+    return -1;
+    
+}
+
+int main() {
+   
+    int even[6] = {2,4,6,8,10,12};
+    int odd[5] = {1,3,5,7,9};
+    
+    int evenindex =  BinarySearch(even,6,12);
+    cout<<"index of 12 is "<<evenindex<<endl;
+    
+    int oddindex =  BinarySearch(odd,5,7);
+    cout<<"index of 7 is "<<oddindex<<endl;
+    
+    
+
+    return 0;
+}
